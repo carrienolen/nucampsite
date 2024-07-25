4af65cf71386a217440869ac95162744b3efa08a
carrienolen <109868339+carrienolen@users.noreply.github.com>
2024-07-24 22:32:47 -0500

    campsitesSlice

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,19 +1,17 @@
-
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
 import { Col, Row } from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from "./campsitesSlice";
 
+const CampsitesList = () => {
+  const campsites = selectAllCampsites();
 
-
-//the set of () after .map are because .map is a function.  The () are around the parameter to the function. The parameter to the function is the (campsite) => {  return ....};  // it's doing this:  { CAMPSITES.map (callbackFunction) } where callbackFunction takes in a campsite and returns some JSX of a Col and CampsiteCard. 
-const CampsitesList= () => {
   return (
     <Row className="ms-auto">
-      {CAMPSITES.map((campsite) => {
+      {campsites.map((campsite) => {
         return (
           <Col md="5" className="m-4" key={campsite.id}>
-            <CampsiteCard campsite={campsite}/>
-          </Col>  
+            <CampsiteCard campsite={campsite} />
+          </Col>
         );
       })}
     </Row>
