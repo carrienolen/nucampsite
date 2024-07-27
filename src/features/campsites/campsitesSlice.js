cb8c2b95e91c08bbd49a6c2aef267f681ff9ca76
carrienolen <109868339+carrienolen@users.noreply.github.com>
2024-07-26 23:42:05 -0500

    Homepage Display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,7 +4,10 @@ export const selectAllCampsites = () => {
   return CAMPSITES;
 };
 
-
 export const selectCampsiteById = (id) => {
   return CAMPSITES.find((campsite) => campsite.id === id);
+};
+
+export const selectFeaturedCampsite = () => {
+  return CAMPSITES.find((campsite) => campsite.featured);
 };
