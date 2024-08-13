210a271e0d4d02db886ea678c879e90d39ff14bc
carrienolen <109868339+carrienolen@users.noreply.github.com>
2024-08-12 23:26:38 -0500

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import {
   Button,
   Modal,
@@ -9,10 +10,13 @@ import {
 } from "reactstrap";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import validateCommentForm from "../../utils/validateCommentForm";
+import { addComment } from "./commentsSlice";
 
 
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const dispatch = useDispatch();
 
   const handleSubmit = (values) => {
     const comment = {
@@ -20,8 +24,10 @@ const CommentForm = ({ campsiteId }) => {
       rating: values.rating,
       author: values.author,
       text: values.commentText,
+      date: new Date(Date.now()).toISOString()
     };
     console.log(comment);
+    dispatch(addComment(comment));
     setModalOpen(false);
   };
 
